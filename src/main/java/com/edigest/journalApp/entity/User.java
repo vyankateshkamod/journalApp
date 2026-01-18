7f008d6a9c9a01daba809f00d8761280e0b7ecbf
vyankateshkamod <vyankateshkamod@gmail.com>
2026-01-18 16:13:37 +0530

    MongoTemplate,Query,Criteria

--- src/main/java/com/edigest/journalApp/entity/User.java
+++ src/main/java/com/edigest/journalApp/entity/User.java
@@ -1,8 +1,6 @@
 package com.edigest.journalApp.entity;
 
-import lombok.Builder;
-import lombok.Data;
-import lombok.NonNull;
+import lombok.*;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.index.Indexed;
@@ -15,12 +13,16 @@ import java.util.List;
 @Document(collection = "users")
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
     @Id
     private ObjectId id ;
     @Indexed(unique = true)
     @NonNull
     private String userName ;
+    private String email ;
+    private boolean sentimentAnalysis;
     @NonNull
     private String password ;
     @DBRef
---
+++ src/test/java/com/edigest/journalApp/repository/UserRepositoryImplTests.java
@@ -0,0 +1,18 @@
+package com.edigest.journalApp.repository;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class UserRepositoryImplTests {
+
+    @Autowired
+    private UserRepositoryImpl userRepository;
+
+    @Test
+    public void testGetUserForSA(){
+        Assertions.assertNotNull(userRepository.getUserForSA());
+    }
+}
